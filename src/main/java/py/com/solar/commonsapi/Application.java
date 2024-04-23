142631a946eef6310ae67b3b03fffbf654b4d074
Angel Maldonado <amaldonado@itae.com.py>
2024-04-23 14:37:29 -0400

    cicd test

--- src/main/java/py/com/solar/commonsapi/Application.java
+++ src/main/java/py/com/solar/commonsapi/Application.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
-
     private static final Logger logger = LoggerFactory.getLogger(Application.class);
     public static void main(String[] args) {
         logger.info("<=== Iniciando {} ... ===>", Application.class.getName());
