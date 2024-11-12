88f366fe4be59f8fd1cd8838105b1e768c5a482f
Rolando Cabral <rcabral@itae.com.py>
2024-11-12 14:39:11 -0300

    Update CommonsAPI.java

--- src/main/java/py/com/solar/commonsapi/controllers/CommonsAPI.java
+++ src/main/java/py/com/solar/commonsapi/controllers/CommonsAPI.java
@@ -9,13 +9,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 <<<<<<< src/main/java/py/com/solar/commonsapi/controllers/CommonsAPI.java
 import org.springframework.web.bind.annotation.*;
-import py.com.solar.commonsapi.models.Country;
+import py.com.solar.commonsapi.models.*;
 =======
 import org.springframework.web.bind.annotation.*;
 >>>>>>> src/main/java/py/com/solar/commonsapi/controllers/CommonsAPI.java
-import py.com.solar.commonsapi.models.Notification;
-import py.com.solar.commonsapi.models.Office;
-import py.com.solar.commonsapi.models.Region;
+import py.com.solar.commonsapi.models.*;
 import py.com.solar.commonsapi.service.CommonService;
 import py.com.solar.commonsapi.utils.BaseAPI;
 
